a4de37e3d2c87eec6ba43fee9992ab3f5d4db84c
vacacoi <vicalf.11@hotmail.com>
2018-04-12 23:48:22 -0500

    Se agrega vista de registro de usuarios.

---
+++ resources/views/manage/update_person.blade.php
@@ -0,0 +1,58 @@
+<div class="table-responsive">
+<table class="table-bordered text-center" id="table-update-person"  width="100%" cellspacing="0">
+  <thead>
+    <tr>
+      <th>@lang("vista.identification_card")</th>
+      <th>@lang("vista.names")</th>
+      <th>@lang("vista.last_name")</th>
+      <th>@lang("vista.telephone")</th>
+      <th>@lang("vista.address")</th>
+      <th>@lang("vista.email")</th>
+      <th>Editar</th>
+      <th>Eliminar</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Tiger Nixon</td>
+      <td>System Architect</td>
+      <td>Tiger Nixon</td>
+      <td>System Architect</td>
+      <td>Tiger Nixon</td>
+      <td>valledelcafe@valle.com</td>
+      <td><button class="btn btn-primary"><i class="fa fa-edit"></i></button></td>
+      <td><button class="btn btn-danger"><i class="fa fa-trash"></i></button></td>
+    </tr>
+    <tr>
+      <td>Tiger Nixon</td>
+      <td>System Architect</td>
+      <td>Tiger Nixon</td>
+      <td>System Architect</td>
+      <td>Tiger Nixon</td>
+      <td>valledelcafe@valle.com</td>
+      <td><button class="btn btn-primary"><i class="fa fa-edit"></i></button></td>
+      <td><button class="btn btn-danger"><i class="fa fa-trash"></i></button></td>
+    </tr>
+    <tr>
+      <td>Tiger Nixon</td>
+      <td>System Architect</td>
+      <td>Tiger Nixon</td>
+      <td>System Architect</td>
+      <td>Tiger Nixon</td>
+      <td>valledelcafe@valle.com</td>
+      <td><button class="btn btn-primary"><i class="fa fa-edit"></i></button></td>
+      <td><button class="btn btn-danger"><i class="fa fa-trash"></i></button></td>
+    </tr>
+    <tr>
+      <td>Tiger Nixon</td>
+      <td>System Architect</td>
+      <td>Tiger Nixon</td>
+      <td>System Architect</td>
+      <td>Tiger Nixon</td>
+      <td>valledelcafe@valle.com</td>
+      <td><button class="btn btn-primary"><i class="fa fa-edit"></i></button></td>
+      <td><button class="btn btn-danger"><i class="fa fa-trash"></i></button></td>
+    </tr>
+  </tbody>
+</table>
+</div>
