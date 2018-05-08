2850fbaa4473191f2b2f5a3151a2c2cf89289e55
vacacoi <vicalf.11@hotmail.com>
2018-05-08 00:14:47 -0500

    Se agrega campo de id de lotes de produccion (Tiempos) a tabla de input lost. Se agrega funcionalidad de ver procesos de trillado de lotes (70% terminado).

---
+++ resources/views/manage/table_process_lots.blade.php
@@ -0,0 +1,63 @@
+
+          <div class="table-responsive">
+            <table class="table-bordered" id="table-estate" width="100%" cellspacing="0" >
+              <thead>
+                <tr>
+                  <th>@lang("vista.th-waiting_lots")</th>
+                  <th>@lang("vista.th-inprocess_lots")</th>
+                </tr>
+              </thead>
+              <tbody>
+                <td>
+                  <form action="production_lots" method="post">
+                    {{ csrf_field() }}
+                    <table class="table table-bordered text-center">
+                    <thead>
+                      <tr>
+                        <th>@lang("vista.id_lot")</th>
+                        <th>@lang("vista.label_details_lots")</th>
+                        <th>@lang("vista.label_enter_production_lost")</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                       @foreach($detailsLost as $detailsLost1)
+                       <tr>
+                         @if($detailsLost1->state == 'P')
+                          <td>{{$detailsLost1->lots_id}}</td>
+                          <td><button id="{{$detailsLost1->lots_id}}" class="btn btn-info" onclick="viewLots(this.id)"><i class="fa fa-eye"></i></button></td>
+                          <td><button type="submit" id="" name="production" value="{{$detailsLost1->lots_id}}" class="btn btn-info" onclick="viewLots(this.id)"><i class=" fa fa-sign-in"></i></button></td>
+                         @endif
+                       </tr>
+                        @endforeach
+                  </tbody>
+                  </table>
+                </form>
+                </td>
+                <td>
+                  <table class="table table-bordered text-center">
+                  <thead>
+                    <tr>
+                      <th>@lang("vista.label_hour_start")</th>
+                      <th>@lang("vista.label_machine_trilla")</th>
+                      <th>@lang("vista.label_machine_desimetric")</th>
+                      <th>@lang("vista.label_machine_electronic")</th>
+                      <th>@lang("vista.label_machine_tostion")</th>
+                      <th>@lang("vista.label_machine_select")</th>
+                      <th>@lang("vista.label_hour_end")</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                     @foreach($detailsLost as $detailsLost2)
+                       @if($detailsLost2->state == 'A')
+                        <td>{{$detailsLost2->lots_id}}</td>
+                        
+                       @endif
+                      @endforeach
+                </tbody>
+                </table>
+                </td>
+
+
+              </tbody>
+            </table>
+          </div>
